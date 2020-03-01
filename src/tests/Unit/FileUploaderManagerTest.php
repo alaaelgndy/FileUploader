3ae6c25c1150174f3b2a7186e52a0fe364b0cff8
alaa elgndy <alaa.elgndy@areebgroup.com>
2020-03-01 19:05:38 +0200

    Started with unit tests

---
+++ src/tests/Unit/FileUploaderManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Elgndy\FileUploader\Tests\Unit;
+
+use Tests\TestCase;
+
+class FileUploaderManagerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function test_function()
+    {
+        $this->assertTrue(true);
+    }
+}
